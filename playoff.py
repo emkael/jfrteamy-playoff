c2cdc9bdab6cbbffa7b825f0277c49f49afac6cf
emkael <emkael@tlen.pl>
2017-08-01 01:46:12 +0200

    Meaningful variable name

--- playoff.py
+++ playoff.py
@@ -6,19 +6,19 @@ from jfr_playoff.filemanager import PlayoffFileManager
 def main():
 
     try:
-        s = PlayoffSettings()
+        settings = PlayoffSettings()
 
-        generator = PlayoffGenerator(s)
+        generator = PlayoffGenerator(settings)
         content = generator.generate_content()
 
-        file_manager = PlayoffFileManager(s)
+        file_manager = PlayoffFileManager(settings)
         file_manager.write_content(content)
         file_manager.copy_scripts()
         file_manager.send_files()
     except:
         print traceback.format_exc()
     finally:
-        if s.interactive:
+        if settings.interactive:
             raw_input('Press any key to continue...')
 
 if __name__ == '__main__':
