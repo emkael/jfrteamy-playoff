9febaf58536bb32a361d6126b8f1327f6a7dcde2
emkael <emkael@tlen.pl>
2018-02-24 11:01:05 +0100

    Fixing remote HTML encoding accordingly to HTML content meta tags

--- jfr_playoff/remote.py
+++ jfr_playoff/remote.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 
 from bs4 import BeautifulSoup as bs
@@ -9,5 +11,14 @@ class RemoteUrl:
     @classmethod
     def fetch(cls, url):
         if url not in cls.url_cache:
-            cls.url_cache[url] = requests.get(url).text
+            request = requests.get(url)
+            encoding_match = re.search(
+                'content=".*;( )?charset=(.*)"',
+                request.content, re.IGNORECASE)
+            if encoding_match:
+                request.encoding = encoding_match.group(2)
+            cls.url_cache[url] = request.text
+            PlayoffLogger.get('remote').info(
+                'content for %s not in cache: retrieved %d bytes',
+                url, len(cls.url_cache[url]))
         return bs(cls.url_cache[url], 'lxml')
